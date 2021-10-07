56b77a9cad45bc08b124acb61090c7c2ef0f0d7a
tokyomanamela <tokyomanamela@192.168.8.111>
2021-10-07 13:23:46 +0200

    banking service

--- src/test/java/com/exergyRestApi/BankService/RetrieveBranch.java
+++ src/test/java/com/exergyRestApi/BankService/RetrieveBranch.java
@@ -13,22 +13,19 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import Utilities.ApiHelper;
 import io.restassured.response.Response;
 
-public class QueryBranchByName extends BankServicePayloads{
+public class RetrieveBranch extends BankServicePayloads{
 
-	public static void  queryBranchByName(ExtentReports extent) throws URISyntaxException, SAXException, IOException, ParserConfigurationException {
+	public static void  retrieveBranch(ExtentReports extent) throws URISyntaxException, SAXException, IOException, ParserConfigurationException {
 		
 		
 		
 		ExtentTest test;
-		test=extent.createTest("Query Branch By Name");
+		test=extent.createTest("Retrieve Branch");
 		Response response;
-		response =  ApiHelper.sendRestPostRequest(ApiHelper.bakingSitUrl,ApiHelper.bakingSubKey, ApiHelper.bakingSubId,QueryBranchByNamePayload, "/QueryBranchByName");
+		response =  ApiHelper.sendRestPostRequest(ApiHelper.bakingSitUrl,ApiHelper.bakingSubKey, ApiHelper.bakingSubId,RetrieveBranchPayload, "/RetrieveBranch");
 		response.prettyPrint();
 		ApiHelper.AssertEquals("Status code" ,"200", String.valueOf(response.statusCode()) , test);
 		test.info( MarkupHelper.createCodeBlock(response.asString(),CodeLanguage.JSON));
-		
-		//validations to be done
-		test.fail("the reponse returns empty array");
 		
 	}
 
--- src/test/java/com/exergyRestApi/BankService/RetrieveVerification.java
+++ src/test/java/com/exergyRestApi/BankService/RetrieveVerification.java
@@ -13,22 +13,19 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import Utilities.ApiHelper;
 import io.restassured.response.Response;
 
-public class QueryBranchByName extends BankServicePayloads{
+public class RetrieveVerification extends BankServicePayloads{
 
-	public static void  queryBranchByName(ExtentReports extent) throws URISyntaxException, SAXException, IOException, ParserConfigurationException {
+	public static void  retrieveVerification(ExtentReports extent) throws URISyntaxException, SAXException, IOException, ParserConfigurationException {
 		
 		
 		
 		ExtentTest test;
-		test=extent.createTest("Query Branch By Name");
+		test=extent.createTest("Retrieve Verification");
 		Response response;
-		response =  ApiHelper.sendRestPostRequest(ApiHelper.bakingSitUrl,ApiHelper.bakingSubKey, ApiHelper.bakingSubId,QueryBranchByNamePayload, "/QueryBranchByName");
+		response =  ApiHelper.sendRestPostRequest(ApiHelper.bakingSitUrl,ApiHelper.bakingSubKey, ApiHelper.bakingSubId,RetrieveVerificationPayload, "/RetrieveVerification");
 		response.prettyPrint();
 		ApiHelper.AssertEquals("Status code" ,"200", String.valueOf(response.statusCode()) , test);
 		test.info( MarkupHelper.createCodeBlock(response.asString(),CodeLanguage.JSON));
-		
-		//validations to be done
-		test.fail("the reponse returns empty array");
 		
 	}
 
