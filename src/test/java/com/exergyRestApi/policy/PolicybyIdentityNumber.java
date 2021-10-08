66accff1792a9167419a797a7d1f25abd073aa87
Richmond <richmondr@stsafrica.com>
2021-10-08 14:04:54 +0200

    Updates

--- src/test/java/com/exergyRestApi/policy/PolicybyIdentityNumber.java
+++ src/test/java/com/exergyRestApi/policy/PolicybyIdentityNumber.java
@@ -11,6 +11,8 @@ import io.restassured.response.Response;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.json.JSONObject;
 import org.xml.sax.SAXException;
 
 
@@ -27,6 +29,28 @@ public static void  policybyIdentityNumber(ExtentReports extent) throws URISynta
 	response.prettyPrint();
 	ApiHelper.AssertEquals("Status code" ,"200", String.valueOf(response.statusCode()) , test);
 	test.info( MarkupHelper.createCodeBlock(response.asString(),CodeLanguage.JSON));
+	
+	//Validations
+	
+	JSONObject innerJson = new JSONObject(response.getBody().asString());
+	System.out.println("++++++++++++++++++++++++"+ApiHelper.getJsonField(innerJson, "benefitPremiums"));
+	
+	String Policy = innerJson.get("policyNumber").toString();
+	ApiHelper.AssertEquals("policyNumber" ,"50015674", Policy, test);
+	
+	String ProductCode = innerJson.get("productCode").toString();
+	ApiHelper.AssertEquals("productCode" ,"60200000", ProductCode, test);
+	
+	String premium = innerJson.getJSONObject("premiumAmount").getString("benefitPremiums");
+	ApiHelper.AssertEquals("benefitPremiums" ,"124.38", premium, test);
+	
+	String arrears = innerJson.get("arrears").toString();
+	ApiHelper.AssertEquals("arrears" ,"-3063.96",arrears, test);
+	
+	String LOBSystem = innerJson.get("lineOfBusinessSystem").toString();
+	ApiHelper.AssertEquals("lineOfBusinessSystem" ,"Mip",LOBSystem, test);
+	
+	
 	
 	
 }
